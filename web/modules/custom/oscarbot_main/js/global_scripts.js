e5f5579e69df748236712f07d657a1d4f8454ad7
ONovas <hola@oscarnovas.com>
2023-06-16 16:05:21 +0200

    feat: Instalar base del módulo custom

---
+++ web/modules/custom/oscarbot_main/js/global_scripts.js
@@ -0,0 +1,24 @@
+/**
+ * @file
+ * Provides oscarbot_main attachment logic.
+ */
+
+(function ($, Drupal, drupalSettings) {
+
+  $(document).ready(function() {
+  })
+
+  /**
+   * Attaches the oscarbot_main behavior
+   *
+   * @type {Drupal~behavior}
+   *
+   * @prop {Drupal~behaviorAttach} attach
+   */
+  Drupal.behaviors.oscarbot_main = {
+    attach: function (context, settings) {
+
+    }
+  };
+
+})(jQuery, Drupal, drupalSettings);
---
+++ web/modules/custom/oscarbot_main/src/lib/handlers/FormAlterHandler.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Drupal\oscarbot_main\lib\handlers;
+
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+
+/**
+ * Conjunto de Handlers usados en el hook form_alter principalmente.
+ */
+class FormAlterHandler {
+
+  use StringTranslationTrait;
+
+  /**
+   * Añade placeholders a los formularios de usuario.
+   *
+   * Login, registro de usuario, recuperar contraseña...
+   *
+   * @param array $form
+   *   Array con el formulario.
+   * @param string $form_id
+   *   Cadena con el identificador del formulario.
+   *
+   * @return array
+   *   Array con el formulario y los placeholders añadidos.
+   */
+  public static function addPlaceholderUserForm(array $form, string $form_id): array {
+    $forms_users_add_placeholder = [
+      'user_login_form',
+      'user_register_form',
+    ];
+
+    if (TRUE === in_array($form_id, $forms_users_add_placeholder)) {
+      if (isset($form['name'])) {
+        $form['name']['#attributes']['placeholder'] = $form['name']['#title'];
+      }
+      if (isset($form['pass'])) {
+        $form['pass']['#attributes']['placeholder'] = $form['pass']['#title'];
+      }
+      if (isset($form['mail'])) {
+        $form['mail']['#attributes']['placeholder'] = $form['mail']['#title'];
+      }
+    }
+
+    return $form;
+  }
+
+}
